d2bc33f3e2158e055fb890afcc8e5f7111f4f2a6
henry <hmarichal93@gmail.com>
2025-01-18 14:00:53 -0300

    mend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Init
+#copy files to cluster uy
+
+rsync -avz  -e "ssh -p 10022" /data/maestria/resultados/deep_cstrd/ henry.marichal@cluster.uy:/clusteruy/home/henry.marichal/datasets/deep_cstrd
--- src/experiments/run_detection_all_dataset.py
+++ src/experiments/run_detection_all_dataset.py
@@ -26,14 +26,14 @@ def generate_pdf(path):
             fig_path += sub_path + "/"
 
         # fig2 = fig_path + "preprocessing_output.png"
-        fig3 = fig_path + "chains.png"
+        fig3 = fig_path + "segmentation.png"
         #fig4 = fig_path + "connect.png"
         #fig5 = fig_path + "postprocessing.png"
         fig6 = fig_path + "output.png"
         x, y = 0, 40
         height = 180
 
-        for fig in [fig6]:
+        for fig in [fig3, fig6]:
             pdf.add_page()
             pdf.cell(0, 0, disco)
             pdf.image(fig, x, y, h=height)
@@ -43,12 +43,12 @@ def generate_pdf(path):
     pdf.output(f"{path}/summary_ipol.pdf", 'F')
 
 def main(root_database = "/data/maestria/resultados/deep_cstrd/pinus_v1/test",  results_path="/data/maestria/resultados/deep_cstrd_pinus_v1_test/deep_cstrd",
-         weights_path="/home/henry/Documents/repo/fing/cores_tree_ring_detection/src/runs/pinus_v1/latest_model.pth"):
+         weights_path="/home/henry/Documents/repo/fing/cores_tree_ring_detection/src/runs/pinus_v1_40_train_12_val/epoch_20/latest_model.pth"):
 
-    metadata_filename = Path(root_database) / 'dataset_ipol.csv'
+    metadata_filename = Path(root_database).parent / 'dataset_ipol.csv'
     images_dir = Path(root_database) / "images/segmented"
     results_path = Path(results_path)
-    results_path.mkdir(exist_ok=True)
+    results_path.mkdir(exist_ok=True, parents=True)
 
     metadata = pd.read_csv(metadata_filename)
     for idx in range(metadata.shape[0]):
@@ -65,13 +65,13 @@ def main(root_database = "/data/maestria/resultados/deep_cstrd/pinus_v1/test",  
 
         cy = row.cy
         cx = row.cx
-        sigma = row.sigma
+        #sigma = row.sigma
         sigma = 3
-        if (img_res_dir / "labelme.json").exists():
-            continue
+        # if (img_res_dir / "labelme.json").exists():
+        #     continue
 
         command = f"python deep_cstrd.py --input {img_filename} --sigma {sigma} --cy {cx} --cx {cy}  --root ./ --output_dir" \
-                  f" {img_res_dir} --hsize 1500 --wsize 1500 --weights_path {weights_path}"
+                  f" {img_res_dir}  --weights_path {weights_path}"
         #
         # command = f"python main.py --input {img_filename} --sigma {sigma} --cy {cx} --cx {cy}  --root ./ --output_dir" \
         #           f" {img_res_dir} --hsize 1500 --wsize 1500 --save_imgs 1"
@@ -84,8 +84,17 @@ def main(root_database = "/data/maestria/resultados/deep_cstrd/pinus_v1/test",  
 
 
 if __name__=='__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Train a U-Net model for image segmentation')
+    parser.add_argument('--dataset_dir', type=str, default="/data/maestria/resultados/deep_cstrd/pinus_v1/test",
+                        help='Path to the dataset directory')
+    parser.add_argument('--results_path', type=str, default="/data/maestria/resultados/deep_cstrd_pinus_v1_test/deep_cstrd",
+                        help='Path to the results directory')
+    parser.add_argument('--weights_path', type=str, default="/home/henry/Documents/repo/fing/cores_tree_ring_detection/src/runs/pinus_v1_40_train_12_val/epoch_20/latest_model.pth",
+                        help='Path to the weights directory')
+    args = parser.parse_args()
 
+    main(args.dataset_dir, args.results_path, args.weights_path)
 
 
 
