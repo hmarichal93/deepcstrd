5733bb25e2b9cf635f0fbe243e5db9e575db409b
henry <hmarichal93@gmail.com>
2025-01-21 15:15:19 -0300

    reducing the specification of the job

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=deep_cstrd
-#SBATCH --ntasks=16
-#SBATCH --mem=100G
+#SBATCH --ntasks=8
+#SBATCH --mem=30G
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --tmp=100G
