44532a6aa3c14ece77b7dc77def93d77d8db5e16
henry <hmarichal93@gmail.com>
2025-01-23 20:27:24 -0300

    cluster sh

--- cluster.sh
+++ cluster.sh
@@ -62,7 +62,7 @@ ls -l $NODE_DATASET_DIR
 # Run the program
 echo "Running the program"
 cd $ROOT_DIR
-python train.py --dataset_dir $NODE_DATASET_DIR  --logs_dir $NODE_LOGS_DIR --model_type $MODEL_TYPE --batch_size $BATCH_SIZE --tile_size $TILE_SIZE
+python deep_cstrd/train.py --dataset_dir $NODE_DATASET_DIR  --logs_dir $NODE_LOGS_DIR --model_type $MODEL_TYPE --batch_size $BATCH_SIZE --tile_size $TILE_SIZE
 
 
 # -------------------------------------------------------
