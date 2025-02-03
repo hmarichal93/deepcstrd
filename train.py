aa8d4f40df8ca814d16a2752fc7094924d34f653
henry <hmarichal93@gmail.com>
2025-02-03 13:34:35 -0300

    added print config to stdout

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 from deep_cstrd.dataset import load_datasets
 from deep_cstrd.utils import save_batch_with_labels_as_subplots
 from deep_cstrd.losses import DiceLoss, Loss
@@ -21,8 +23,8 @@ def save_config(logs_dir, dataset_root, tile_size, overlap, batch_size, lr, numb
     #     os.system(f"rm -r {logs_dir}")
 
     Path(logs_dir).mkdir(parents=True, exist_ok=True)
-
-    with open(f"{logs_dir}/config.txt", "w") as f:
+    config_path = Path(logs_dir) / "config.txt"
+    with open(str(config_path), "w") as f:
         f.write(f"dataset_root: {dataset_root}\n")
         f.write(f"tile_size: {tile_size}\n")
         f.write(f"overlap: {overlap}\n")
@@ -35,6 +37,11 @@ def save_config(logs_dir, dataset_root, tile_size, overlap, batch_size, lr, numb
         f.write(f"model_type: {model_type}\n")
         f.write(f"encoder: {encoder}\n")
         f.write(f"debug: {debug}\n")
+
+    #load config file txt with numpy
+    config = np.loadtxt(config_path, delimiter=":", dtype=str)
+    print(config)
+
 
 def configure_optimizer(model, lr, number_of_epochs, step_size = None, gamma = None):
     optimizer = optim.AdamW(model.parameters(), lr=lr)
