b16d6a08583977a5af355fe8dc5a64001b61c00b
henry <hmarichal93@gmail.com>
2025-07-09 20:23:50 -0300

    trasnfer learning

---
+++ cluster_transfer_learning.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH --job-name=deep_cstrd
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=30G
+#SBATCH --time=48:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --tmp=100G
+#SBATCH --mail-user=henry.marichal@fing.edu.uy
+#SBATCH --gres=gpu:a40:1
+#SBATCH --partition=normal
+#SBATCH --qos=gpu
+
+# Cargar módulos y activar el entorno
+source /etc/profile.d/modules.sh
+source /clusteruy/home/henry.marichal/miniconda3/etc/profile.d/conda.sh
+conda activate deep_cstrd
+
+# Variables
+ROOT_DIR=$1
+HOME_DATASET_DIR=$2/$4
+HOME_RESULTADOS_DIR=$3
+DATASET_NAME1=$4
+DATASET_NAME2=$5
+NODE_DATASET_DIR=/scratch/henry.marichal/
+NODE_RESULTADOS_DIR=$NODE_DATASET_DIR/results
+EPOCHS=$6
+TILESIZE=0
+TEST_SIZE=$7
+BATCHSIZE=$8
+MODEL_TYPE=$9
+# Función para verificar el resultado de un comando
+check_command_result() {
+    echo "Executing: $@"
+    "$@"
+    if [ $? -ne 0 ]; then
+        echo "Error: El comando falló."
+        exit 1
+    fi
+}
+
+# Preparar directorios
+check_command_result rm -rf $NODE_DATASET_DIR $NODE_RESULTADOS_DIR
+check_command_result mkdir -p $NODE_DATASET_DIR $NODE_RESULTADOS_DIR
+
+# Copiar dataset al disco local del nodo
+check_command_result cp -r $HOME_DATASET_DIR $NODE_DATASET_DIR
+
+# Entrenar el modelo
+cd $ROOT_DIR
+for i in {1..5}; do
+    ITERATION_FOLDER="$NODE_RESULTADOS_DIR/iteration_$i"
+    check_command_result mkdir -p $ITERATION_FOLDER
+    check_command_result python main.py train --dataset_dir $NODE_DATASET_DIR/$DATASET_NAME1 --logs_dir $ITERATION_FOLDER \
+        --batch_size $BATCHSIZE --tile_size $TILESIZE --encoder resnet18 --number_of_epochs $EPOCHS \
+        --boundary_thickness 3 --augmentation 1 --test_size 0 --model_type $MODEL_TYPE --freeze_encoder 1
+
+    MODEL_WEIGHTS=$(find ${ITERATION_FOLDER} -name best_model.pth)
+    check_command_result python main.py train --dataset_dir $NODE_DATASET_DIR/$DATASET_NAME2 --logs_dir $ITERATION_FOLDER \
+        --batch_size $BATCHSIZE --tile_size $TILESIZE --encoder resnet18 --number_of_epochs $EPOCHS \
+        --boundary_thickness 3 --augmentation 1 --test_size $TEST_SIZE --model_type $MODEL_TYPE --freeze_encoder 1 \
+        --weights_path $MODEL_WEIGHTS
+
+    check_command_result mkdir -p $HOME_RESULTADOS_DIR
+    check_command_result cp -r $ITERATION_FOLDER/* $HOME_RESULTADOS_DIR
+done
